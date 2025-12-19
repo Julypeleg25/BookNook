f938e478a968c41e8dcb91362ad245e1fc4d058b
julypeleg25 <julypeleg@gmail.com>
2025-12-19 13:00:08 +0200

    wip login page - css needed

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,30 +1,51 @@
-import { Button, TextField, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import loginIcon from "../assets/login.jpg";
 
 const Login = () => {
   return (
-    <div>
-      <h6 style={{ justifySelf: "start", left: 0 }}>Welcome to BookNook</h6>
-      <h6>Get Started in a journey of reading</h6>
-      <Button />
-      <Typography>Login here!</Typography>
-      <Typography>username</Typography>
-      <TextField placeholder="enter your username"></TextField>
-      <Typography>password</Typography>
-      <TextField placeholder="enter your password"></TextField>
-      <img
-        src={loginIcon}
-        alt="Login Icon"
-        width={"400rem"}
-        style={{ justifySelf: "end" }}
-      />
-      <Typography variant="body2">
-        New to BookNook? <a href="/register">Register</a>
-      </Typography>
-      <Button color="success" variant="contained">
-        hello
-      </Button>
-    </div>
+    <Box display="flex">
+      <Box flex={1} display="flex" margin={"2rem"}>
+        <div style={{}}>
+          <h3>Welcome to BookNook</h3>
+          <Typography variant="body2">Please Enter your details</Typography>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "1rem",
+              marginTop: "1rem",
+              marginBottom: "1rem",
+            }}
+          >
+            <Typography>username</Typography>
+            <TextField placeholder="enter your username"></TextField>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "1rem",
+              marginTop: "1rem",
+              marginBottom: "1rem",
+            }}
+          >
+            {" "}
+            <Typography>password</Typography>
+            <TextField placeholder="enter your password"></TextField>
+          </div>
+
+          <Typography variant="body2">
+            new to BookNook? <a href="/register">Register</a>
+          </Typography>
+          <Button color="success" variant="contained">
+            hello
+          </Button>
+        </div>
+      </Box>
+      <Box flex={1} bgcolor={"white"}>
+        <img src={loginIcon} style={{ width: "70%", height: "70%" }} />
+      </Box>
+    </Box>
   );
 };
 
