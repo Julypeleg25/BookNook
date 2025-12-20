a9bb18d0b5bde7ba5844ac0d715bd6bffc5c722a
July Peleg <julypeleg@gmail.com>
2025-12-20 02:28:23 +0200

    added book post card data

--- frontend/src/components/BookPostCard.tsx
+++ frontend/src/components/BookPostCard.tsx
@@ -5,22 +5,28 @@ import {
   CardContent,
   CardHeader,
   CardMedia,
-  Collapse,
+  Chip,
   IconButton,
+  Rating,
   Typography,
 } from "@mui/material";
 import { red } from "@mui/material/colors";
 import { BiShare } from "react-icons/bi";
 import { CgMoreVertical } from "react-icons/cg";
 import { GrFavorite } from "react-icons/gr";
+import type { BookPost } from "../models/Book";
 
-const BookPostCard = () => {
+interface BookPostCardProps {
+  book: BookPost;
+}
+
+const BookPostCard = ({ book }: BookPostCardProps) => {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ width: "22rem" }}>
       <CardHeader
         avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
+          <Avatar>
+            <img src={book.user.avatarUrl} alt={book.user.name} />
           </Avatar>
         }
         action={
@@ -28,29 +34,31 @@ const BookPostCard = () => {
             <CgMoreVertical />
           </IconButton>
         }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        title={book.book.title}
+        subheader={book.createdDate.toDateString()}
       />
-      <CardMedia
-        component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
-      />
+      <CardMedia component="img" height="260rem" image={book.imageUrl} />
       <CardContent>
         <Typography variant="body2" sx={{ color: "text.secondary" }}>
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
+          {book.description}
         </Typography>
       </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <GrFavorite />
-        </IconButton>
-        <IconButton aria-label="share">
-          <BiShare />
-        </IconButton>
+      <CardActions
+        style={{
+          justifyContent: "space-between",
+          display: "flex",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <div>
+          <IconButton>
+            <GrFavorite />
+          </IconButton>
+          {book.likes}
+        </div>
+          <Chip label={book.book.genre}></Chip>
+        <Rating value={book.rating} precision={0.5} readOnly />
       </CardActions>
     </Card>
   );
--- frontend/src/pages/Explore.tsx
+++ frontend/src/pages/Explore.tsx
@@ -1,10 +1,20 @@
+import { Box } from "@mui/material";
 import BookPostCard from "../components/BookPostCard";
+import { bookPosts } from "../exampleData";
+import type { BookPost } from "../models/Book";
 
 const Explore = () => {
   return (
-    <div>
-      <BookPostCard />
-    </div>
+    <Box
+      display="grid"
+      gridTemplateColumns={{ xs: "1fr", sm: "1fr 1fr", md: "1fr 1fr 1fr 1fr" }}
+      gap={"2rem"}
+      padding={"1rem"}
+    >
+      {bookPosts.map((post: BookPost) => (
+        <BookPostCard key={post.id} book={post} />
+      ))}
+    </Box>
   );
 };
 export default Explore;
