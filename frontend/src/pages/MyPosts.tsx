9dee1c9cbeb824789daad6acd6bfbbd3b1ddace8
yammicka <julypeleg@gmail.com>
2026-02-21 19:11:16 +0200

    my posts with infinite loader

--- frontend/src/pages/MyPosts.tsx
+++ frontend/src/pages/MyPosts.tsx
@@ -4,18 +4,45 @@ import { Box } from "@mui/material";
 import { useMemo } from "react";
 import { useInfiniteLoader } from "@hooks/useInfiniteLoader";
 import type { BookPost } from "@models/Book";
+import { useQuery } from "@tanstack/react-query";
+import useUserStore from "@/state/useUserStore";
+import { userReviewService } from "@/api/services/userReviewService";
 
 const user_id = "u1";
 const BATCH_SIZE = 5;
 
-const MyPosts = () => {
-  const exampleUserPosts = useMemo(
-    () => bookPosts.filter((post) => post.user.id === user_id),
-    []
-  );
 
+const MyPosts = () => {
+  const {user: {username
+    }} = useUserStore()
+ const { data: reviews = [], isLoading, isError, refetch } = useQuery({
+        queryKey: ["allReviews", username],
+        queryFn: () => userReviewService.getAllReviews(
+          0, 
+          "", 
+          username,
+          0,
+          ""
+        )})
+
+        const bookPosts: BookPost[] = useMemo(() => reviews.map((r: any) => ({
+              id: r._id,
+              book: r.book,
+              user: r.user,
+              createdDate: r.createdDate,
+              description: r.review,
+              rating: r.rating,
+              imageUrl: r.picturePath,
+              likes: r.likes,
+              comments: r.comments.map((c: any) => ({
+                  id: c._id,
+                  user: c.user,
+                  createdDate: c.createdAt,
+                  content: c.comment
+              })),
+          })), [reviews]);
   const { visibleItems, loaderRef } = useInfiniteLoader<BookPost>({
-    items: exampleUserPosts,
+    items: bookPosts,
     batchSize: BATCH_SIZE,
     rootMargin: "50px",
   });
@@ -38,7 +65,7 @@ const MyPosts = () => {
         <FullBookPostCard key={post.id} post={post} />
       ))}
 
-      {visibleItems.length < exampleUserPosts.length && <Box ref={loaderRef} />}
+      {visibleItems.length < bookPosts.length && <Box ref={loaderRef} />}
     </Box>
   );
 };
